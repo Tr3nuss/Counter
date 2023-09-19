54ff0c15ca25eeee80b32301df479b8ec54454c4
Tr3nuss <ivashkaa2006@mail.ru>
2023-09-19 23:16:47 +0300

    second

---
+++ app.js
@@ -0,0 +1,7 @@
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const result = document.querySelector('.result');
+
+plus.addEventListener('click', function(){
+    result.innerText == ++result.innerText;
+})
